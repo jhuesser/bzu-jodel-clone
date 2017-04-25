2cf06e00145f59c302d102feb1699e8f0775b15b
jhuesser <jonas.huesser+github@gmail.com>
2017-04-25 07:36:40 +0200

    commented signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+//Load functions, config and pass metadata
 $title = "Sign up | SocialDomayn";
 $stylesheet = "login.css";
 include 'functions/header.php';
@@ -12,23 +13,27 @@ $config = include('config.php');
 <?php
 
 if(isset($_GET['register'])) {
+//User wants to register
 
-
+//set local values for easy handling
  $error = false;
  $username = $_POST['username'];
  $password = $_POST['password1'];
  $password2 = $_POST['password2'];
-if(isset($_POST['g-recaptcha-response']))
+	if(isset($_POST['g-recaptcha-response']))
+	//captcha is solved
           $captcha=$_POST['g-recaptcha-response'];
 
-        if(!$captcha){
+   if(!$captcha){
+			//captcha is not solved
           $errorMsg = $config->app_msgs['catpcha_not_solved'];
           
         }
 
- $captachresp =json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdozRgTAAAAALNEFAspM4WP66rIrWxikjFSnHfK&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+ $captachresp =json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$config->recaptcha_secret ."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
         if($captachresp['success'] == false)
         {
+			//captcha is false
           $errorMsg = $config->app_msgs['captcha_fail'];
         }
         else
@@ -36,6 +41,7 @@ if(isset($_POST['g-recaptcha-response']))
           
         
  if(strlen($password) == 0) {
+	 //password is not set
  	$errorMsg = $config->app_msgs['set_passwd'];
  $error = true;
  }
@@ -46,33 +52,39 @@ if(isset($_POST['g-recaptcha-response']))
 
 
  
- //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+ //check if username is already taken
  if(!$error) { 
  
 	$caller = "https://jodel.domayntec.ch/api.php/jodlers?transform=1&filter=jodlerHRID,eq," . $username;
 	$resp = getCall($caller);
  if($resp !== '{"jodlers":[]}') {
+	 //recived empty JSON
+	 //TODO: make this nice (code styling, json handling)
 	$errorMsg = $config->app_msgs['nametaken'];
  $error = true;
  } 
  }
  
- //Keine Fehler, wir können den Nutzer registrieren
+ //no errors, user can be registered
  if(!$error) { 
+ //hash password & salt it
  $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
+ //remove special chars, to avoid injection
  $username = htmlspecialchars($username, ENT_QUOTES);
  
- 
+ //register the user
  $caller ="https://jodel.domayntec.ch/api.php/jodlers";
  $postdata = "{\n  \"jodlerHRID\": \"$username\",\n  \"karma\": 50,\n  \"account_state\": 1,\n  \"passphrase\": \"$password_hash\"\n}";
  $userid = postCall($caller, $postdata);
-
+//response is ID of the new user
  if(is_numeric($userid)) { 
+ //if ID is a number, user is created
 	$successMsg = $config->app_msgs['acc_created'];
  header('Location: https://jodel.domayntec.ch/login.php');
  $showFormular = false;
  } else {
+	 //something went wrong
  	$errorMsg = $config->app_msgs['general_error'];
  }
  } 
@@ -81,6 +93,7 @@ if(isset($_POST['g-recaptcha-response']))
 }
 
 if(isset($errorMsg)) {
+	//Show error message
  ?>
 
  <div class="alert alert-danger" role="alert">
@@ -103,7 +116,7 @@ if(isset($successMsg)) {
 }
 ?>
 
-
+<!-- signup form -->
 <div class="container">
 	<div class="wrapper">
 		<form action="?register=1" method="post" name="signupform" class="form-signup">       
@@ -117,6 +130,8 @@ if(isset($successMsg)) {
 			 
 			  <button class="btn btn-lg btn-block"  name="Submit" value="Signup" type="Submit"><?php echo $config->login_strings['signup'];?></button>  			
 		</form>
+		<!-- link to login page -->
 		<p><?php echo $config->login_strings['acc_exists']; ?></p>			
 	</div>
 </div>
+<!-- end form -->
