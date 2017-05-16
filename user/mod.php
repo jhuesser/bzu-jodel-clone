9561139997ca17db73b9568dd00e5cb722e27e31
jhuesser <github@jhuesser.ch>
2017-05-16 08:45:54 +0200

    base structure of mod.php

--- user/mod.php
+++ user/mod.php
@@ -15,3 +15,42 @@
 	//set up working variables
 	$userid = $_SESSION['userid'];
 	$apiroot = $config->apiUrl;
+
+?>
+<div id="top"></div>
+<!-- main menu -->
+<ul class="nav justify-content-center">
+	<li class="nav-item">
+		<a class="nav-link" href="../user.php"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+	</li>
+	<li class="nav-item">
+    <a class="nav-link" href="javascript:window.location.reload();"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../jodels.php"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
+  </li>
+  
+</ul>
+<!-- end main menu -->
+<div class="test"></div>
+<?php
+	if(isset($_SESSION['errorMsg'])) {
+		//show error msg
+ 		?>
+		<div class="alert alert-danger" role="alert">
+  		<strong>Holy guacamole!</strong> <?php echo $_SESSION['errorMsg'];?>
+		</div>
+		<?php
+	}
+	?>
+	<div class="container">
+		<h1>
+			<?php echo "Hello " . $_SESSION['username'];?>
+		</h1>
+	</div>
+	<?php
+		//get JSON of all colors, save it in PHP array
+		$reporturl = $apiroot . "reports?transform=1";
+		$reportjson = getCall($reporturl);
+		$reports = json_decode($reportjson, true);
+	?>
