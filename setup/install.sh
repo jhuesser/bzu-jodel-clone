2abb86291ba0326fea41c5c677e9b4de8a375036
Hüsser Jonas <idch-hjn@immodata.ch>
2017-05-24 14:41:00 +0200

    some corerctions

--- setup/install.sh
+++ setup/install.sh
@@ -24,12 +24,12 @@ fi
 
 find $workingdir -name '*.php' -print | cpio -pvdumB $wwwroot
 
-read -p "Enter your mysql username" username
-read -p "Enter your mysql password" password
-read -p "Enter the mysql hostname" hostname
+read -p "Enter your mysql username: " username
+read -p -s "Enter your mysql password: " password
+read -p "Enter the mysql hostname: " hostname
 
-sed -i -e 's/myuser/${username}/g' ${workingdir}/setup/database_setup.sql
-sed -i -e 's/myuser/${username}/g' ${workingdir}/setup/database_setup.sql
+sed -i -e "s/myuser/${username}/g" ${workingdir}/setup/database_setup.sql
+sed -i -e "s/myhost/${hostname}/g" ${workingdir}/setup/database_setup.sql
 
 mysql -h $hostname -u $username -p$password < ${workingdir}/setup/database_setup.sql
 
